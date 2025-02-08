3429bad074d90b5721e6adbad62f84dd771869c9
Romario <romaef23@gmail.com>
2025-02-07 20:17:59 -0500

    [ADD] Cube animated

---
+++ src/app/components/engine/engine.component.ts
@@ -0,0 +1,27 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { EngineService } from '../../services/engine/engine.service';
+
+@Component({
+  selector: 'app-engine',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './engine.component.html',
+})
+export class EngineComponent implements OnInit {
+  @ViewChild('rendererCanvas', { static: true })
+  public rendererCanvas!: ElementRef<HTMLCanvasElement>;
+
+  public constructor(private engineService: EngineService) {}
+
+  public ngOnInit(): void {
+    this.engineService.createScene(this.rendererCanvas);
+    this.engineService.animate();
+  }
+}
